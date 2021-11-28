25373a344fa5a54bf54a69685a0f233ecc7d99b6
Sergey Gavrilenko <gryroach@gmail.com>
2021-11-28 21:31:55 +0300

    Solve lesson 12. Images in admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Category, Movie, Member, MovieShots, Genre, Rating, RatingStar, Review
 # Register your models here.
 
@@ -10,10 +12,22 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 # отображение отзывов в информации о фильмах
-class ReviewInLine(admin.StackedInline):
+class ReviewInLine(admin.TabularInline):
     model = Review
     readonly_fields = ('name', 'email')
     extra = 1
+
+
+class MovieShotsInLine(admin.TabularInline):
+    model = MovieShots
+    extra = 1
+    readonly_fields = ('get_image',)
+
+    # Отображение картикок в админке
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="150" height="120">')
+
+    get_image.short_description = "Изображение"
 
 
 @admin.register(Movie)
@@ -22,27 +36,49 @@ class MovieAdmin(admin.ModelAdmin):
                     'display_actor', 'category', 'display_genre', 'premier', 'url', 'draft')
     list_filter = ('category', 'year')
     search_fields = ('title', 'category__name')
-    inlines = [ReviewInLine]
+    inlines = [MovieShotsInLine, ReviewInLine]
     # Перенос панели сохранения наверх
     save_on_top = True
     # Сохранение как нового объекта без очищения полей
     save_as = True
     fieldsets = ((None, {'fields': ('title', 'tagline', 'description', ('year', 'country', 'premier'), 'genre')}),
                  ('Актеры и режисеры', {'fields': (('director', 'actor'),)}),
-                 (None, {'fields': (('budget', 'fees_USA', 'feel_world'), 'poster')}),
+                 (None, {'fields': (('budget', 'fees_USA', 'feel_world'), ('poster', 'get_image'))}),
                  ('Прочая информация', {'classes': ('collapse',), 'fields': ('category', 'url', 'draft')}))
     # изменение поля в списке
     list_editable = ('draft',)
 
+    # Отображение постера
+    readonly_fields = ('get_image',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.poster.url} width="100" height="150">')
+
+    get_image.short_description = "Постер"
+
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ('name', 'age', 'description', 'image')
+    list_display = ('name', 'age', 'description', 'get_image')
+    readonly_fields = ('get_image',)
+
+    # Отображение картикок в админке
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="100" height="120">')
+
+    get_image.short_description = "Изображение"
 
 
 @admin.register(MovieShots)
 class MovieShotsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'image', 'movie')
+    list_display = ('title', 'description', 'get_image', 'movie')
+    readonly_fields = ('get_image',)
+
+    # Отображение картикок в админке
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.image.url} width="150" height="120">')
+
+    get_image.short_description = "Изображение"
 
 
 @admin.register(Genre)
@@ -62,3 +98,7 @@ admin.site.register(RatingStar)
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ('email', 'name', 'text', 'parent', 'movie')
     readonly_fields = ('email', 'name')
+
+
+admin.site.site_title = "Администрирование фильмов"
+admin.site.site_header = "Администрирование фильмов"
